871b2cd6a4fdc4c0d5d38e9d4db264e086cf79c7
Amal Yulianto <amal.yulianto@gmail.com>
2025-01-29 19:54:58 +0700

    fixing story1.json incompatibility

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -17,7 +17,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
   _navigateToHome() async {
     await Future.delayed(Duration(seconds: 3), () {});
-    Get.to(() => HomePage());
+    Get.offAll(() => HomePage());
   }
 
   @override
